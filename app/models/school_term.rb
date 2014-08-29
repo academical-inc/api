786167262f851bc602567e375a1e0f66e303b5bd
Juan Tejada <juans.tejada@gmail.com>
2014-08-29 17:28:15 -0400

    Adds test suite for SchoolTerm and fixes model accordingly

--- app/models/school_term.rb
+++ app/models/school_term.rb
@@ -10,12 +10,17 @@ module Academical
       field :end_date, type: Date
 
       validates_presence_of :name, :start_date, :end_date
-      validate :dates_correct?
+      validate :check_dates_are_correct
 
       embedded_in :school, inverse_of: :terms
 
       def dates_correct?
         start_date < end_date
+      end
+
+      def check_dates_are_correct
+        errors.add(:start_date, "can't be greater than end_date") \
+          if not dates_correct?
       end
 
     end
---
+++ spec/models/school_term_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe SchoolTerm do
+
+  describe 'instantiation' do
+    let(:term) { build(:school_term) }
+
+    it 'should instantiate a SchoolTerm' do
+      expect(term.class.name).to eq("#{base_model_name}SchoolTerm")
+    end
+  end
+
+  describe '#dates_correct?' do
+    let(:term) { build(:school_term) }
+    let(:inv_term) { build(:school_term, invalid: true) }
+
+    it 'should return true if start_date is before end_date' do
+      expect(term.dates_correct?).to be(true)
+    end
+
+    it 'should return false if start_date is after end_date' do
+      expect(inv_term.dates_correct?).to be(false)
+    end
+  end
+
+  describe 'validations' do
+    let(:term) { build(:school_term, invalid:true) }
+
+    it 'should not be valid when dates are not correct' do
+      expect(term).not_to be_valid
+    end
+  end
+
+end
