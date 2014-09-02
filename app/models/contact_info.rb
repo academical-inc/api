cb410744b267c5eec882c3516cbfb79491e96844
Juan Tejada <juans.tejada@gmail.com>
2014-09-02 12:24:37 -0400

    Updates contact_info model

--- app/models/contact_info.rb
+++ app/models/contact_info.rb
@@ -1,17 +1,15 @@
 module Academical
   module Models
-
     class ContactInfo
 
       include Mongoid::Document
 
       field :name, type: Hash, default: {}
-      field :role
+      field :title
       field :phone
 
       embedded_in :school
 
     end
-
   end
 end
