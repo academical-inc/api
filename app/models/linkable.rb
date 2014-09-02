dbb800f7dd3dee7f0d7748e34e87a5fc9e2915a2
Juan Tejada <juans.tejada@gmail.com>
2014-09-02 13:03:30 -0400

    Cleaned the code on Linkable a little

--- app/models/linkable.rb
+++ app/models/linkable.rb
@@ -9,9 +9,8 @@ module Academical
         end
       end
 
-      def update_links(linked_fields: self.linked_fields,
-                       base_url: self.link_to_self,
-                       links: self.links)
+      def update_links
+        base_url = link_to_self
         links[:self] = base_url
         linked_fields.each do |linked_field|
           links[linked_field] = "#{base_url}/#{linked_field}"
@@ -22,9 +21,10 @@ module Academical
         raise Exceptions::MethodMissingError, "Must implement #linked_fields"
       end
 
-      def link_to_self(class_name: self.class.name, _id: self._id)
+      def link_to_self
+        class_name = self.class.name
         resource_url_name = class_name.demodulize.underscore.pluralize
-        "/#{resource_url_name}/#{_id}"
+        "/#{resource_url_name}/#{self._id}"
       end
 
     end
