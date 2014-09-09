e2b15be9115a76e711ba34b97cd883fa8ce1f291
Juan Tejada <juans.tejada@gmail.com>
2014-09-09 02:03:13 -0400

    Adds validations and indexes to School model

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -7,14 +7,25 @@ module Academical
       include Linkable
 
       field :name, type: String
-      field :total_sections, type: Integer
       field :total_credits,  type: Float
+      field :total_sections, type: Integer
       field :share_id, type: String
       embeds_one  :term, class_name: "SchoolTerm"
       embeds_many :personal_events, class_name: "Event"
       belongs_to  :school, index: true
       belongs_to  :student, index: true
       has_and_belongs_to_many :sections, index: true
+
+      validates_presence_of :name, :total_credits, :total_sections, :term,
+                            :school, :student
+
+      index({name: 1})
+      index({total_credits: 1})
+      index({total_sections: 1})
+      index({school: 1, name:1})
+      index({school: 1, total_credits: 1})
+      index({school: 1, total_sections: 1})
+      index({:school=>1, "personal_events.name"=>1}, {sparse: true})
 
       def linked_fields
         [:student, :sections, :school]
