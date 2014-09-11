1a5d7b339c9d7cf6f157e6b84cc54ef8fbfd6a3d
Juan Tejada <juans.tejada@gmail.com>
2014-09-11 14:48:37 -0400

    Updates mongoid initializer:
    - Overrides serialize_hash for all Mongoid Documents to get a proper
      json representation:
      - Replaces _id field for id in root documents (not embedded)
      - Remove any id (or _id) field from embedded documents
    - Adds docs
    
      (this took way longer than it should've )
      https://gist.github.com/jstejada/b33d1184ffdfc7b4e8a3

--- config/initializers/init_mongoid.rb
+++ config/initializers/init_mongoid.rb
@@ -1,6 +1,31 @@
+# Monkeypatching BSON::Object
 module BSON
   class ObjectId
+    # These aliases override default JSON representation of an ObjectId
+    # Spits a string id '540f51de6d61632c74020000', instead of
+    # { "$oid": '540f51de6d61632c74020000' }
     alias :to_json :to_s
     alias :as_json :to_s
+  end
+end
+
+# Monkeypatching Mongoid::Document
+module Mongoid::Document
+
+  # Overrides ActiveModel::Serialization#serializable_hash for Mongoid Documents
+  # If needed, please refer to the original method documentation.
+  # Returns serialized_hash of any Mongoid::Document with following conditions:
+  #
+  #  - For a root document (not embedded) the `:_id` field is replaced for
+  #    `:id` field
+  #  - For an embedded document :_id field is completely excluded
+  #
+  # It gracefully keeps any other :only, :except, :methods and :include
+  # options originally passed in the `options` hash
+  def serializable_hash(options = nil)
+    hash = super options
+    hash["id"] = self._id if not self.embedded?
+    hash.delete("_id")
+    hash
   end
 end
