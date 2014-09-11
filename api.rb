d4a5f71d2df048a166e4598713eb2b96db45a1ca
Juan Tejada <juans.tejada@gmail.com>
2014-09-11 16:15:09 -0400

    Fixes minor error with I18n load_path

--- api.rb
+++ api.rb
@@ -47,7 +47,7 @@ module Academical
     configure do
       I18n.enforce_available_locales = true
       I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
-      I18n.load_path = Dir[File.join(root, 'config/locales', '*.yml')]
+      I18n.load_path += Dir[File.join(root, 'config/locales', '*.yml')]
       I18n.available_locales = [:en, :es]
       I18n.backend.load_translations
     end
