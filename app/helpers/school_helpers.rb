335c0424b3250021632234e405d245d0010b3dee
Juan Tejada <juans.tejada@gmail.com>
2014-09-15 17:21:08 -0400

    Adds new method to SchoolHelpers

--- app/helpers/school_helpers.rb
+++ app/helpers/school_helpers.rb
@@ -2,8 +2,17 @@ module Academical
   module Helpers
     module SchoolHelpers
 
+      def schools(where: nil)
+        School.all if where.blank?
+        School.where(where)
+      end
+
+      def school(id: params[:school_id])
+        @school ||= School.find(id) || halt(404, "Not Found! :)")
+      end
+
       def create_school(params)
-        @school = School.create! params
+        School.create! params
       rescue Mongoid::Errors::Validations
         halt 400, json({:status => "failed"})
       end
