74272a624c9ab864418fe57a23ce9cfc8d35d06c
Juan Tejada <juans.tejada@gmail.com>
2014-09-22 16:32:28 -0400

    Updates Student model Factory

--- spec/factories/students.rb
+++ spec/factories/students.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
 
+  sequence :username do |n|
+    "user#{n}"
+  end
+
   factory :student do
-    username "user1"
-    email "user1@gmail.com"
+    username { generate(:username) }
+    email { "#{username}@gmail.com" }
     last_login Date.new(2015, 1, 1)
     login_provider "Google"
     school { build(:school) }
