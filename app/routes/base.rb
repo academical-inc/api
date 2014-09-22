7cc47ed546af9215dd53240fef73d4ab3f89d7dc
Juan Tejada <juans.tejada@gmail.com>
2014-09-22 17:55:59 -0400

    Raises error again inside error handling if in test env:
    - To be able to see the error log while testing

--- app/routes/base.rb
+++ app/routes/base.rb
@@ -23,18 +23,22 @@ module Academical
 
       error do
         dump_errors! env['sinatra.error']
+        raise env['sinatra.error'] if settings.test?
         json_error 500
       end
 
       error ParameterMissingError do
+        raise env['sinatra.error'] if settings.test?
         json_error 400
       end
 
       error Mongoid::Errors::DocumentNotFound do
+        raise env['sinatra.error'] if settings.test?
         json_error 404, message: "The resource was not found"
       end
 
       error Mongoid::Errors::Validations do
+        raise env['sinatra.error'] if settings.test?
         json_error 400, message: "The provided data is incomplete or malformed"
       end
 
