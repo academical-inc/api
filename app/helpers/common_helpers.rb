65707e61ee9984a58f6a69592fdb6d418e5b9a7e
Juan Tejada <juans.tejada@gmail.com>
2014-09-22 16:29:05 -0400

    Creates new utils class to generate json response hashes
    - Adds ResponseUtils class
    - Uses from inside common helpers to generate responses

--- app/helpers/common_helpers.rb
+++ app/helpers/common_helpers.rb
@@ -7,36 +7,13 @@ module Academical
       end
 
       def json_error(code, ex: env['sinatra.error'], message: nil, errors: {})
-          settings.set :environment, :production
-        message = if not message.blank?
-          message
-        elsif not ex.blank? and not settings.production?
-          ex.message
-        else
-          "Something went wrong. Please try again later"
-        end
-
-        response_hash = {
-          success: false,
-          message: message,
-          errors: errors
-        }
-
-        response_hash[:backtrace] = ex.backtrace if settings.development?\
-          and not ex.blank?
-
+        response_hash = ResponseUtils.error_hash settings.production?,
+          settings.development?, ex: ex, message: message, errors: errors
         halt code, json(response_hash)
       end
 
       def json_response(data, code: 200)
-        response_hash = if data.respond_to? :each
-          {
-            data: data.as_json
-          }
-        else
-          data.as_json
-        end.merge({success: true})
-
+        response_hash = ResponseUtils.success_hash data
         status code
         json response_hash
       end
---
+++ lib/utils/response_utils.rb
@@ -0,0 +1,42 @@
+module Academical
+  module Utils
+    class ResponseUtils
+
+      def self.error_hash(prod, dev, ex: nil, message:nil, errors: {})
+        message = if not message.blank?
+          message
+        elsif not ex.blank? and not prod
+          ex.message
+        else
+          "Something went wrong. Please try again later"
+        end
+
+        response_hash = {
+          success: false,
+          message: message,
+          errors: errors
+        }
+        response_hash[:backtrace] = ex.backtrace if dev and not ex.blank?
+
+        response_hash
+      end
+
+      def self.success_hash(data)
+        response_hash = if data.respond_to?(:key?)
+          if data.key?(:data) or data.key?("data")
+            data
+          else
+            {data: data}
+          end
+        elsif data.respond_to? :each
+          {data: data.as_json(root: :data)}
+        else
+          data.as_json root: :data
+        end.merge({success: true})
+
+        response_hash
+      end
+
+    end
+  end
+end
