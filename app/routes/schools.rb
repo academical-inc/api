99f6f100521a68a63c9950ed4f58ad2805a62019
Juan Tejada <juans.tejada@gmail.com>
2014-09-22 16:32:08 -0400

    Updates to new json_response helper

--- app/routes/schools.rb
+++ app/routes/schools.rb
@@ -5,22 +5,22 @@ module Academical
       helpers Helpers::SchoolHelpers
 
       get '/schools' do
-        json schools
+        json_response schools
       end
 
       @base_school_route = '/schools/:school_id'
       get @base_school_route do
-        json school
+        json_response school
       end
 
       School.linked_fields.each do |field|
         get "#{@base_school_route}/#{field}" do
-          json school.send(field.to_sym)
+          json_response school.send(field.to_sym)
         end
       end
 
       post '/schools' do
-        json create_school(params[:data])
+        json_response create_school
       end
 
     end
