2830a9c22f46793882121f8b6fbd3c1f7d83304b
Juan Tejada <juans.tejada@gmail.com>
2015-02-23 12:31:05 -0500

    Adds spec for new students endpoint behavior

--- spec/routes/students_spec.rb
+++ spec/routes/students_spec.rb
@@ -11,5 +11,47 @@ describe Academical::Routes::Students do
 
   it_behaves_like Academical::Routes::ModelRoutes, to_update, to_remove,
     [:schedules], [:school, :registered_schedule]
+
+  describe "get /students/:resource_id/schedules" do ||
+    let(:student) { resource_to_create }
+    before(:each) do
+      student.save!
+      schedules = create_list(
+        :schedule,
+        2,
+        student: student,
+        school: student.school
+      )
+      sections = create_list(:section, 2, :with_events)
+      schedules.each do |sched|
+        sched.sections = sections
+        sched.save!
+      end
+      student.schedules = schedules
+      student.save!
+    end
+
+    it "retreives student's schedules with embedded sections when requested" do
+      get "/students/#{student.id}/schedules?include_sections"
+      schedules = json_response
+      schedules.each do |schedule|
+        expect(schedule).to have_key("sections")
+        expect(schedule["sections"].count).to equal(2)
+      end
+    end
+
+    it "retreives student's schedules with expanded section events when requested" do
+      get "/students/#{student.id}/schedules?include_sections&expand_section_events"
+      schedules = json_response
+      schedules.each do |schedule|
+        expect(schedule).to have_key("sections")
+        expect(schedule["sections"].count).to equal(2)
+        schedule["sections"].each do |section|
+          section["events"].each {|e| expect(e).to have_key("expanded")}
+        end
+      end
+    end
+
+  end
 end
 
