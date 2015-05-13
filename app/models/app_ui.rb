dcf97771474de6628c91f469835f5cabb806c612
Juan Tejada <juans.tejada@gmail.com>
2015-05-13 13:07:27 -0400

    Uodates AppUi and spec

--- app/models/app_ui.rb
+++ app/models/app_ui.rb
@@ -5,8 +5,8 @@ module Academical
 
       include Mongoid::Document
 
-      field :search_filters, type: Hash, default: {}
-      field :summary_fields, type: Hash, default: {}
+      field :search_filters, type: Array, default: []
+      field :summary_fields, type: Array, default: []
       field :search_result_fields, type: Hash, default: {}
       field :info_fields, type: Array, default: []
 
