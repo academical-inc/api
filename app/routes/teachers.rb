79b6bf0386c4bbe7176cd4094bba47103dc38732
Juan Tejada <juans.tejada@gmail.com>
2015-06-12 16:12:16 -0400

    Adds auth protection to routes that are admin only:
    - Schools, Teachers and Main are admin only now

--- app/routes/teachers.rb
+++ app/routes/teachers.rb
@@ -2,6 +2,12 @@ module Academical
   module Routes
     class Teachers < Base
 
+      before "/teachers*" do
+        authorize! do
+          is_admin?
+        end
+      end
+
       include ModelRoutes
 
     end
