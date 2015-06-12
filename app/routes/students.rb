fa8fefe808e9a7084cfba57fe4fb0078bf39b586
Juan Tejada <juans.tejada@gmail.com>
2015-06-12 16:15:30 -0400

    Major updates to Students routes and permissions:
    - Partially reimplements some endpoints that require special permissions
    - Still includes ModelRoutes
    - Permissions cover all endpoints
    - POST endpoint is special to create or update user (depending on login)

--- app/routes/students.rb
+++ app/routes/students.rb
@@ -2,6 +2,49 @@ module Academical
   module Routes
     class Students < Base
 
+      def students_auth(method=nil, path=nil)
+        if request.delete? or request.put?
+          is_admin?
+        elsif request.get?
+          if path != "/students"
+            is_student?
+          else
+            is_admin?
+          end
+        else
+          is_student?
+        end
+      end
+
+      def is_current_student?(student)
+        is_admin? or (is_student? and student == current_student)
+      end
+
+      before "/students*" do
+        if request.delete? or request.put? or (request.get? and request.path_info == "/students")
+          authorize! do
+            is_admin?
+          end
+        end
+      end
+
+      Student.linked_fields.each do |field|
+        before "/students/:resource_id/#{field}" do
+          authorize! do
+            is_current_student? resource
+          end
+        end
+      end
+
+      get "/students/:resource_id" do
+        student = resource
+        authorize! do
+          is_current_student? student
+        end
+        json_response student
+      end
+
+      # TODO Improve this
       get "/students/:resource_id/schedules" do
         inc_secs   = contains? :include_sections
         expand_evs = contains? :expand_events
@@ -27,10 +70,32 @@ module Academical
         json_response schedules
       end
 
+      post "/students" do
+        data = extract! :data
+        id            = extract :id, data
+        auth0_user_id = extract :auth0_user_id, data
+
+        student = Student.any_of(
+          {id: id},
+          {auth0_user_id: auth0_user_id}
+        ).first
+
+        if student.blank?
+          authorize! 403 do
+            is_admin? or is_student?
+          end
+          json_response create_resource(data), code: 201
+        else
+          authorize! 403 do
+            is_current_student? student
+          end
+          student.update_attributes! data
+          json_response student
+        end
+      end
+
       include ModelRoutes
 
     end
   end
 end
-
-
