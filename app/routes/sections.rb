9945397b6897da07545d7bbd4460bacc36873b7b
Juan Tejada <juans.tejada@gmail.com>
2015-06-27 19:35:42 -0400

    Updates /sections/search endpoint:
    - Adds authorization
    - Updates extraction of parameters
    - Passes appropriate options for json generation, to include
    corequisites in response

--- app/routes/sections.rb
+++ app/routes/sections.rb
@@ -10,16 +10,20 @@ module Academical
       end
 
       get "/sections/search" do
-        # authorize! do
-        #   is_admin? or is_student?
-        # end
-        query   = extract(:q) || "*"
+        authorize! do
+          is_admin? or is_student?
+        end
+        query   = extract(:q)
+        query   = "*" if query.blank?
         school  = extract(:school)
         term    = extract(:term)
         filters = extract(:filters)
         filters = MultiJson.load filters if not filters.blank?
 
-        json_response Section.autocompl_search(query, school, term, filters )
+        json_response(
+          Section.autocompl_search(query, school, term, filters),
+          options: {properties: :public}
+        )
       end
 
       include ModelRoutes
