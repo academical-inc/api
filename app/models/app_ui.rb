b27fbb7fa2dd78393eadcad9a02bf240c0bfa489
Juan Tejada <juans.tejada@gmail.com>
2015-07-07 11:06:17 -0400

    Updates AppUi:
    - Changes info fields from array to hash

--- app/models/app_ui.rb
+++ app/models/app_ui.rb
@@ -8,7 +8,7 @@ module Academical
       field :search_filters, type: Array, default: []
       field :summary_fields, type: Array, default: []
       field :search_result_fields, type: Hash, default: {}
-      field :info_fields, type: Array, default: []
+      field :info_fields, type: Hash, default: {}
 
       embedded_in :school
 
